b332af6145805448e73b117a2349da74897b6f72
endes <endes@disroot.org>
2024-02-09 16:48:09 +0000

    wip sflu device

--- hw/ssi/soc09s_sflu.c
+++ hw/ssi/soc09s_sflu.c
@@ -10,26 +10,120 @@
 #include "migration/vmstate.h"
 #include "trace.h"
 
-REG32(STATE, 0x0)
-    FIELD(STATE, BUSY, 0, 1)
-REG32(CTRL0, 0x2)
 REG32(CTRL1, 0x4)
     FIELD(CTRL1, XCE_CHIP_0, 0, 1)
     FIELD(CTRL1, XCE_CHIP_1, 8, 1)
 REG32(CTRL2, 0x6)
     FIELD(CTRL2, XCE_CHIP_2, 0, 1)
     FIELD(CTRL2, XCE_CHIP_3, 8, 1)
-REG32(TRIGGER0, 0x8)
-REG32(TRIGGER1, 0xA)
-REG32(PORT_CTRL, 0x10)
-REG32(BUSY_EXT, 0x20)
-REG32(SDR_WSIZE0, 0x30)
-REG32(SDR_WSIZE1, 0x32)
-REG32(SDR_ADDR0, 0x34)
-REG32(SDR_ADDR1, 0x36)
-REG32(SI_BUFF, 0x50)
-REG32(SO_BUFF0, 0x60)
-REG32(SO_BUFF1, 0x62)
-REG32(SO_BUFF2, 0x64)
-REG32(SO_BUFF3, 0x66)
-REG32(CTRL, 0x102)
+
+
+static void sflu_xfer(Soc09sSflu *s) {
+    while (!fifo8_is_empty(&s->tx_fifo) && !fifo8_is_full(&s->rx_fifo)) {
+        uint8_t val = fifo8_pop(&s->tx_fifo);
+        fifo8_push(&s->rx_fifo, val);
+    }
+}
+
+static void sflu_read(void *opaque, hwaddr addr, uint64_t *val, unsigned size)
+{
+    Soc09sSflu *s = opaque;
+    switch (addr) {
+    case SFLU_STATE:
+        *val = FIELD_DP32(val, SFLU_STATE, SFLU_BUSY, 0);
+        break;
+    case SFLU_CTRL1:
+        *val = FIELD_DP32(val, SFLU_CTRL1, XCE_CHIP_0, s->selected_chip == 0);
+        *val = FIELD_DP32(val, SFLU_CTRL1, XCE_CHIP_1, s->selected_chip == 1);
+        break;
+    case SFLU_CTRL2:
+        *val = FIELD_DP32(val, SFLU_CTRL2, XCE_CHIP_2, s->selected_chip == 2);
+        *val = FIELD_DP32(val, SFLU_CTRL2, XCE_CHIP_3, s->selected_chip == 3);
+        break;
+    case SFLU_SO_BUFF1:
+        *val = fifo8_pop(&s->rx_fifo);
+        break;
+    default:
+        printf("sflu: unhandled read from 0x%"HWADDR_PRIx"\n", addr);
+        break;
+    }
+}
+
+static void sflu_write(void *opaque, hwaddr addr, uint64_t val, unsigned size)
+{
+    Soc09sSflu *s = opaque;
+    switch (addr) {
+    case SFLU_CTRL1:
+        sflu_xfer(s);
+
+        if (FIELD_EX32(val, SFLU_CTRL1, XCE_CHIP_0)) {
+            s->selected_chip = 0;
+        } else if (FIELD_EX32(val, SFLU_CTRL1, XCE_CHIP_1)) {
+            s->selected_chip = 1;
+        }
+        break;
+    case SFLU_CTRL2:
+        sflu_xfer(s);
+        
+        if (FIELD_EX32(val, SFLU_CTRL2, XCE_CHIP_2)) {
+            s->selected_chip = 2;
+        } else if (FIELD_EX32(val, SFLU_CTRL2, XCE_CHIP_3)) {
+            s->selected_chip = 3;
+        }
+        break;
+    case SFLU_SI_BUFF:
+        if (!fifo8_is_full(&s->tx_fifo)) {
+            fifo8_push(&s->tx_fifo, val);
+        } else {
+            printf("sflu: tx fifo full\n");
+            //TODO: set busy bit
+        }
+        
+        break;
+    default:
+        printf("sflu: unhandled write to 0x%"HWADDR_PRIx"\n", addr);
+        break;
+    }
+}
+
+static const MemoryRegionOps sflu_ops = {
+    .read = sflu_read,
+    .write = sflu_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+static void soc09s_sflu_realize(DeviceState *dev, Error **errp)
+{
+    SysBusDevice *sbd = SYS_BUS_DEVICE(dev);
+    Soc09sSflu *s = SOC09SFLU(dev);
+
+    memory_region_init_io(&s->iomem, OBJECT(s), &sflu_ops, s, "sflu", 0x1000);
+    sysbus_init_mmio(sbd, &s->iomem);
+    //sysbus_init_irq(sbd, &s->irq);
+    s->ssi = ssi_create_bus(dev, "ssi");
+
+    fifo8_create(&s->rx_fifo, 256);
+    fifo8_create(&s->tx_fifo, 256);
+}
+
+static void soc09s_sflu_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+
+    //dc->reset = soc09s_sflu_reset;
+    dc->realize = soc09s_sflu_realize;
+}
+
+static const TypeInfo soc09s_sflu_info = {
+    .name          = TYPE_SOC09S_SFLU,
+    .parent        = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof(Soc09sSflu),
+    .class_init    = soc09s_sflu_class_init,
+};
+
+static void soc09s_sflu_register_types(void)
+{
+    type_register_static(&soc09s_sflu_info);
+}
+
+type_init(soc09s_sflu_register_types)
--- include/hw/ssi/soc09s_sflu.h
+++ include/hw/ssi/soc09s_sflu.h
@@ -7,12 +7,35 @@
 #include "qom/object.h"
 
 #define TYPE_SOC09S_SFLU "soc09s-sflu"
+OBJECT_DECLARE_SIMPLE_TYPE(Soc09sSflu, SOC09SFLU)
+
+#define SFLU_STATE 0x00
+#define SFLU_CTRL0 0x02
+#define SFLU_CTRL1 0x04
+#define SFLU_CTRL2 0x06
+#define SFLU_TRIGGER0 0x08
+#define SFLU_TRIGGER1 0x0A
+#define SFLU_PORT_CTRL 0x10
+#define SFLU_BUSY_EXT 0x20
+#define SFLU_SDR_WSIZE0 0x30
+#define SFLU_SDR_WSIZE1 0x32
+#define SFLU_SDR_ADDR0 0x34
+#define SFLU_SDR_ADDR1 0x36
+#define SFLU_SI_BUFF 0x50
+#define SFLU_SO_BUFF0 0x60
+#define SFLU_SO_BUFF1 0x62
+#define SFLU_SO_BUFF2 0x64
+#define SFLU_SO_BUFF3 0x66
+#define SFLU_CTRL 0x102
 
 typedef struct Soc09sSflu {
     /* <private> */
     SysBusDevice parent_obj;
+    uint16_t selected_chip;
 
     /* <public> */
+    Fifo8 rx_fifo;
+    Fifo8 tx_fifo;
     MemoryRegion mmio;
     SSIBus *ssi;
 } Soc09sSflu;
